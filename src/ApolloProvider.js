d9e4aef90c636fe34cabf57d93e58803cf1a2dae
kriztiank <kriztiank@gmail.com>
2020-09-03 13:53:31 +0200

    changes for deployment

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,8 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:5000'
+  // uri: 'http://localhost:5000'
+  uri: 'https://fierce-forest-87237.herokuapp.com/'
 });
 
 const authLink = setContext(() => {
